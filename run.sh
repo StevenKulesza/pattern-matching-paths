2ffd5cbf384d121753d5e7fe5a13dc1d380ea30e
steven <stevenn.kulesza@gmail.com>
2019-06-10 19:05:17 -0400

    bash script

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
- 
+#!/bin/sh    
+rm -rf tests/outputs
+mkdir tests/outputs
+cat tests/inputs/sample_input | node index.js > tests/outputs/sample_output
+cat tests/inputs/input | node index.js > tests/outputs/output
