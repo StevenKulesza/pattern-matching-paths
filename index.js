e75d67a8b4958cd3d9896d803226aa99a1621c00
steven <stevenn.kulesza@gmail.com>
2019-06-11 10:11:22 -0400

    set up initial class

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-process.stdin.on('data', function (data) {
-    process.stdout.write(data);
-  });
+const fs = require('fs');
+const readline = require('readline');
+const stream = require('stream');
+const instream = fs.createReadStream("./tests/inputs/sample_input.txt");
+const outstream = new stream;
+const rl = readline.createInterface(instream, outstream);
+
+
+class PatternMatchingPaths {
+    constructor(input){
+        this.input = input;
+    }
+
+    getInputs() {
+    }
+
+    run() {
+        rl.on('line', async line => {
+        console.log(line)
+        });
+
+        rl.on('close', function() {
+        console.log('done')
+    });
+
+    }
+};
+
+
+const pmp = new PatternMatchingPaths(instream);
+pmp.run()
